dd8da290a530aabe76fb32d9d0393183e3d29a42
GC1C_16_トミタ_アヤナ <k022g1021@g.neec.ac.jp>
2023-04-20 17:02:35 +0900

    AL3_01_02

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,19 +1,91 @@
 #include "GameScene.h"
+#include "AxisIndicator.h"
+#include "ImGuiManager.h"
+#include "PrimitiveDrawer.h"
 #include "TextureManager.h"
 #include <cassert>
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete model_;
+	delete debugCamera_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	// 2D\d
+	// 読み込み処理
+	textureHandle2D_ = TextureManager::Load("sample.png");
+	// 生成
+	sprite_ = Sprite::Create(textureHandle2D_, {10, 10});
+
+	// 3d
+	// 読み込み処理
+	textureHandle3D_ = TextureManager::Load("zuiko.png");
+	// 3dモデルの生成
+	model_ = Model::Create();
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	// サウンドデータの読み込み処理
+	soundHandle_ = audio_->LoadWave("fanfare.wav");
+	// 音声再生
+	audio_->PlayWave(soundHandle_, true);
+
+	// ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+
+	// デバッグカメラも生成
+	debugCamera_ = new DebugCamera(1280, 720);
+	// 軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	// 軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	// スプライトの今の座標を取得
+	Vector2 pos_ = sprite_->GetPosition();
+	// 座標を{2,1}移動
+	pos_.x += 2.0f;
+	pos_.y += 1.0f;
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(pos_);
+
+	// スペースキーを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		// 音声停止
+		audio_->StopWave(voiceHandle_);
+	}
+
+	// デバッグテキストの表示
+	ImGui::Text("Tomita Ayana %d %d %d", 2002, 8, 20);
+	// 別ウィンドウでの表示
+	ImGui::Begin("Debug1");
+	ImGui::Text("Tomita Ruki %d %d %d", 2005, 10, 23);
+	ImGui::End();
+
+	// 実行中に値を変更できるウィンドウ
+	ImGui::Begin("DebugNumSlider");
+	// float3入力ボックス
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	// floatスライダー
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
+
+	// デモウィンドウの表示を有効化
+	ImGui::ShowDemoWindow();
+
+	// デバッグカメラの更新
+	debugCamera_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -41,10 +113,14 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle3D_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
+
+	// ラインを描画する
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
 
 #pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
@@ -53,6 +129,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
